4e58242da45e8a9cf2d7d498bc94b052ca87be85
qadeer0343 <qadeer0343@gmail.com>
2024-07-23 00:25:21 +0500

    variable data types etc 07/18/2024

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ console.log(a)
    const a = "hi"
     console.log(a);
 }
+
+// if condition explained and practice
+ if (condition){
+// if condition checks for true condition and if condition is ture then if block starts
+// if condition false then it goes for else statement
+     
+
+ }
+
+// if else if ...
+// if 
+
+ 
+
+
+ 
