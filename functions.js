9d1762dc1e353f82e4e18fbb49df7f80b87538e3
qadeer0343 <qadeer0343@gmail.com>
2024-07-24 11:56:01 +0500

    07/24/24 learned ternary operator in depth

---
+++ functions.js
@@ -0,0 +1,13 @@
+// function evenOdd(){
+//     console.log(arguments);
+// }
+
+// evenOdd(22, 33, 44, 55)
+
+ let a = 4
+ let b = 10
+ let c = 12
+ let d = 15
+
+ let result =  a<b?(a>c?a:c):(b<c?c:b)
+ console.log(result);
--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-const a = "hello"
-console.log(a)
+const a = "hello";
+console.log(a);
 
 {
-    
-   const a = "hi"
-    console.log(a);
+  const a = "hi";
+  console.log(a);
 }
 
 // if condition explained and practice
- if (condition){
+//  if (condition){
 // if condition checks for true condition and if condition is ture then if block starts
 // if condition false then it goes for else statement
-     
 
- }
+//  }
 
 // if else if ...
-// if 
-
- 
-
-
- 
+if (10 < 5) {
+  console.log("10<5");
+} else if (10 < 4) {
+  console.log("10<4");
+} else if (10 < 3) {
+  console.log("10<3");
+} else {
+  console.log("else is running");
+}
