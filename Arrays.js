4b93dc030dc422757fb6987bab693707f60e24ce
qadeer0343 <qadeer0343@gmail.com>
2024-07-26 01:25:10 +0500

    07/25/2024  arrays and objects

---
+++ Arrays.js
@@ -0,0 +1,34 @@
+let funn = () => {
+  return true;
+};
+
+let array = [5, "myName", null, true, undefined, [4, 5], { age: 30 }, funn];
+console.log(array);
+
+// if(array.includes("abc")){
+//     array[array.indexOf("myName")] = "Abeed"
+// }
+
+array[array.indexOf("myName") > -1 ? array[array.indexOf("myName")] = "Qadeer":array.length = 1]
+
+console.log(array);
+
+// console.log(array);
+
+//  array.push("abdullah")
+
+// // console.log(array);
+// let newArray = array.push("Qadeer")
+
+// console.log(newArray);
+// console.log(array.push("israr"));
+// array.indexOf(5);
+
+// const person = {
+//     Name : "Abdullah", 
+//     Age : 24,
+//     gender: "male",
+     
+// }
+
+
--- functions.js
+++ functions.js
@@ -4,10 +4,21 @@
 
 // evenOdd(22, 33, 44, 55)
 
- let a = 4
- let b = 10
- let c = 12
- let d = 15
+//  let a = 4
+//  let b = 10
+//  let c = 12
+//  let d = 15
 
- let result =  a<b?(a>c?a:c):(b<c?c:b)
- console.log(result);
+//  let result =  a<b?(a>c?a:c):(b<c?c:b)
+//  console.log(result);
+// evenOdd(7);
+// function evenOdd (p1,p2){
+//     console.log("p==>",p1);
+//     console.log("p==>",p2);
+// }
+
+let evenn = function evenOdd(p1, p2) {
+  console.log("p==>", p1);
+  console.log("p==>", p2);
+}
+evenn(7, 10);
